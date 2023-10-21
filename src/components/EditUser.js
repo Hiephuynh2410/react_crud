6931f68cad75ef604b650f05ffa802af534738fb
HiepHuynh <huynhhiepvan1998@gmail.com>
2023-10-21 09:54:55 +0700

    done

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -4,79 +4,81 @@ import { useNavigate, useParams} from "react-router-dom";
 
 export default function ListUser() {
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [inputs, setInputs] = useState([])
-  const {id} = useParams();
-  useEffect(() => {
-    getUsers();
-  }, []);
+    const [inputs, setInputs] = useState([])
 
-  function getUsers() {
-      axios.get(`http://localhost:80/api/user/${id}`)
-          .then(function (response) {
-              console.log(response.data);
-              setInputs(response.data);
-      });
-}
+    const {id} = useParams();
 
-  const handleChange = (event) => {
-      const name = event.target.name;
-      const value = event.target.value;
-      setInputs(values => ({...values, [name]: value}));
-  };
+    useEffect(() => {
+        getUser();
+    }, []);
 
-  const handleSubmit = (event) => {
-      event.preventDefault();
+    function getUser() {
+        axios.get(`http://localhost:80/api/user/${id}`)
+            .then(function (response) {
+                console.log(response.data);
+                setInputs(response.data);
+        });
+    }
 
-      axios.put(`http://localhost:80/api/user/${id}/edit`, inputs)
-          .then(function (response) {
-            console.log(response.data);
-            navigate('/');
-      });
-      console.log(inputs);
-  };
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}));
+    };
 
-  return (
-      <div>
-          <h1>Edit User</h1>
-          <form onSubmit={handleSubmit}>
-              <table cellSpacing="10">
-                  <tbody >
-                      <tr>
-                          <th>
-                              <label>Name: </label>
-                          </th>
-                          <td>    
-                              <input value={inputs.name} type="text" name ="name" onChange={handleChange }/>
-                          </td>
-                      </tr>
-                      <tr>
-                          <th>
-                              <label>Email: </label>
-                          </th>
-                          <td>
-                              <input value={inputs.email} type="text" name ="email" onChange={handleChange }/>
-                          </td>
-                      </tr>
-                      <tr>
-                          <th>
-                              <label>Mobile: </label>
-                          </th>
-                          <td>
-                              <input value={inputs.mobile} type="text" name ="mobile" onChange={handleChange }/>
-                          </td>
-                      </tr>
-                      
-                      <tr>
-                          <td align="right" colSpan="2">
-                              <button>Save</button>
-                          </td>
-                      </tr>
-                  </tbody>
-              </table>
-              
-          </form>
-      </div>
-  )
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.put(`http://localhost:80/api/user/${id}/edit`, inputs)
+            .then(function (response) {
+                console.log(response.data);
+                navigate('/');
+        });
+        console.log(inputs);
+    };
+
+    return (
+        <div>
+            <h1>Edit User</h1>
+            <form onSubmit={handleSubmit}>
+                <table cellSpacing="10">
+                    <tbody >
+                        <tr>
+                            <th>
+                                <label>Name: </label>
+                            </th>
+                            <td>    
+                                <input value={inputs.name} type="text" name ="name" onChange={handleChange }/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <label>Email: </label>
+                            </th>
+                            <td>
+                                <input value={inputs.email} type="text" name ="email" onChange={handleChange }/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <label>Mobile: </label>
+                            </th>
+                            <td>
+                                <input value={inputs.mobile} type="text" name ="mobile" onChange={handleChange }/>
+                            </td>
+                        </tr>
+                        
+                        <tr>
+                            <td align="right" colSpan="2">
+                                <button>Save</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+            </form>
+        </div>
+    )
 }
--- src/components/ListUser.js
+++ src/components/ListUser.js
@@ -15,7 +15,13 @@ export default function ListUser()  {
                 setUsers(response.data);
         });
     }
-    
+
+    const deleteUser= (id) => {
+        axios.delete(`http://localhost:80/api/user/${id}/delete`).then(function (response) {
+            console.log(response.data);
+            getUsers();
+        });
+    }
     return  (
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <table >
@@ -39,7 +45,7 @@ export default function ListUser()  {
                             <td>{user.mobile}</td>
                             <td>
                                 <Link to={`user/${user.id}/edit`} style={{marginRight:"10px"}}>Edit</Link>
-                                <button>Delete</button>
+                                <button onClick={() => deleteUser(user.id)}>Delete</button>
                             </td>
                         </tr>
                     )}
